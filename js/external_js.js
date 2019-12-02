778a59a8b16be06fc019a4c1012ffe4c401bf2db
Carlos Delgado III <carlos.delgado.dev@gmail.com>
2019-12-02 09:23:20 -0600

    morning warmup js

--- js/external_js.js
+++ js/external_js.js
@@ -6,3 +6,6 @@ var userInput = prompt('Favorite Color?');
 console.log('Great, ' + userInput + ' is my favorite color too!');
 
 
+
+// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+//     Use an alert to show the results of each problem.
