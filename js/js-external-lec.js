11258aca584dc1cc7539b1a5432e5e6a626ee850
Carlos Delgado III <carlos.delgado.dev@gmail.com>
2019-12-02 10:14:04 -0600

    js-html lecture

--- js/js-external-lec.js
+++ js/js-external-lec.js
@@ -1,1 +1,36 @@
-console.log("Hello, World! From our external js file!");
+// This is for the user interactions
+
+//TODO: Comment out previous scripts
+
+/* *******************
+*		ALERTS
+* ********************/
+
+
+//TODO TOGETHER: Write an alert ---> alert("Message goes here.");
+
+
+//TODO: Write an alert with an alert message
+
+
+
+/* *******************
+*		Confirm
+* ********************/
+
+
+//TODO TOGETHER: Write a confirm interaction and console.log the result
+// the following line will show the OK/CANCEL confirm dialog
+var confirm = confirm("are you sure you want to exit");
+console.log(confirm);
+// TODO: Select the OK option. View the result. Select the confirm option. View the result.
+
+
+/* *******************
+*		Prompts
+* ********************/
+// TODO TOGETHER: Write a prompt interaction. Console.log the result
+​var userInput = prompt("Please enter your name.");
+console.log(userInput);
+// TODO: Write an additional prompt. Console.log the result
+
