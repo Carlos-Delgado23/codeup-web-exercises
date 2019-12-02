df42e87354ed547b56fe500b1349da93af628272
Carlos Delgado III <carlos.delgado.dev@gmail.com>
2019-12-02 08:35:01 -0600

    added inline and external js exercise files.

---
+++ js/external_js.js
@@ -0,0 +1,8 @@
+console.log('Hello from external JavaScript');
+
+alert("Welcome to my Web");
+
+var userInput = prompt('Favorite Color?');
+console.log('Great, ' + userInput + ' is my favorite color too!');
+
+
