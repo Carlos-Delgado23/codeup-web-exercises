4eb98384daf88612bd91e3e764dfb02fa8486348
Carlos Delgado III <carlos.delgado.dev@gmail.com>
2019-12-02 14:20:51 -0600

    exercises for external js

--- js/external_js.js
+++ js/external_js.js
@@ -1,28 +1,43 @@
 // console.log('Hello from external JavaScript');
 //
-// alert("Welcome to my Web");
+// alert("Welcome to my Website");
 //
 // var userInput = prompt('Favorite Color?');
-// console.log('Great, ' + userInput + ' is my favorite color too!');
+// alert('Great, ' + userInput + ' is my favorite color too!');
 
 
 
-// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
-//     Use an alert to show the results of each problem.
+//
+// var littleMermaid = Number(prompt("How many days do you want to rent Little Mermaid?"));
+// var brotherBear = Number(prompt("How many days do you want to rent Brother Bear?"));
+// var hercules = Number(prompt("How many days do you want to rent Hercules?"));
+// var rentalRate = Number(prompt("What is the rental rate?"));
+//
+// alert("Your total is: " + ((littleMermaid + brotherBear + hercules) * rentalRate) );
+//
+//
+//
+// var googleRate = Number(prompt("What is the rate for Google?"));
+// var googleMoney = Number(prompt("How many hours did you work?"));
+// var amaRate = Number(prompt("What is the rate for Amazon?"));
+// var amaMoney = Number(prompt("How many hours did you work?"));
+// var fbRate = Number(prompt("What is the rate for FB?"));
+// var fbMoney = Number(prompt("How many hours did you work?"));
+//
+// alert("Your take home amount is: " + ((amaMoney * amaRate) + (fbMoney * fbRate) + (googleMoney * googleRate)));
+//
 
-var littleMermaid = prompt("How many days do you want to rent Little Mermaid?");
-var brotherBear = prompt("How many days do you want to rent Brother Bear?");
-var hercules = prompt("How many days do you want to rent Hercules?");
-
-alert("Your total is: " + ((littleMermaid * 3) + (brotherBear * 3) + (hercules * 3)) );
 
 
+// var classAt8AM = confirm("Schedules do not conflict");
+// var currentClassSize = confirm("Class is not full");
+// var canRegister = (currentClassSize && classAt8AM);
+// alert("Student can enroll: " + canRegister);
 
-var googleRate = prompt("What is the rate?");
-var amaRate = prompt("What is the rate?");
-var fbRate = prompt("What is the rate?");
 
-var googleMoney = prompt("How many hours did you work?");
-var amaMoney = prompt("How many hours did you work?");
-var fbMoney = prompt("How many hours did you work?");
-alert("Your take home amount is: " + ((amaMoney * amaRate)+ (fbMoney * fbRate) + (googleMoney * googleRate)));
+var hasKrogerCard = confirm ("Are you a premium member?");
+var cartSize = Number(prompt("How many items in your cart?"));
+var offerValid = confirm("is offer still valid?");
+var canHaveDisc = offerValid && (hasKrogerCard || cartSize > 2);
+
+alert(canHaveDisc);
