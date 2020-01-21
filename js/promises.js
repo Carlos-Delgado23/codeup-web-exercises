d72ba844fa68c74925065278c2e537fdd10277cb
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-01-21 14:24:40 -0600

    promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,22 +1,30 @@
-// let wait = function (num) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Here is your data: ...');
-//         }, num);
-//     })
-// };
-//
-// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+let wait = (num) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, num);
+    })
+};
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 
+function lastCommit(username) {
+let url = `https://api.github.com/users/${username}/events/public`
 
-fetch('https://api.github.com/users',
-    {headers: {
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': gitHubTokenUsers
 
-            }}).then(function (username) {
-    console.log(username);
-});
+    fetch(url, {
+            headers: {
+                'Accept': 'application/vnd.github.v3+json',
+                'Authorization': `token ${gitHubTokenUsers}`
+            }
+        })
+        .then(data => data.json())
+        .then(data => data.find(event => event.type === "PushEvent"))
+        .then(event => event.created_at)
+        .then(date => console.log(date));
+}
+
+console.log(lastCommit('carlos-delgado23'));
